eb0ce4bcb10635acdb9bfd625d714b96be2c7030
Mauri Pino Ricci <mauripino69@gmail.com>
2024-10-17 18:11:44 -0300

    agregue imagen default

--- src/seeds/products/products-mock.ts
+++ src/seeds/products/products-mock.ts
@@ -4,83 +4,95 @@ export const ProductsMock = [
         "description": "The best smartphone in the world",
         "price": 199.99,
         "stock": 12,
-        "category": "smartphone"
+        "category": "smartphone",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Samsung Galaxy S23",
         "description": "The best smartphone in the world",
         "price": 150.0,
         "stock": 12,
-        "category": "smartphone"
+        "category": "smartphone",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Motorola Edge 40",
         "description": "The best smartphone in the world",
         "price": 179.89,
         "stock": 12,
-        "category": "smartphone"
+        "category": "smartphone",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Samsung Odyssey G9",
         "description": "The best monitor in the world",
         "price": 299.99,
         "stock": 12,
-        "category": "monitor"
+        "category": "monitor",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "LG UltraGear",
         "description": "The best monitor in the world",
         "price": 199.99,
         "stock": 12,
-        "category": "monitor"
+        "category": "monitor",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Acer Predator",
         "description": "The best monitor in the world",
         "price": 150.0,
         "stock": 12,
-        "category": "monitor"
+        "category": "monitor",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Razer BlackWidow V3",
         "description": "The best keyboard in the world",
         "price": 99.99,
         "stock": 12,
-        "category": "keyboard"
+        "category": "keyboard",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Corsair K70",
         "description": "The best keyboard in the world",
         "price": 79.99,
         "stock": 12,
-        "category": "keyboard"
+        "category": "keyboard",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Logitech G Pro",
         "description": "The best keyboard in the world",
         "price": 59.99,
         "stock": 12,
-        "category": "keyboard"
+        "category": "keyboard",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Razer Viper",
         "description": "The best mouse in the world",
         "price": 49.99,
         "stock": 12,
-        "category": "mouse"
+        "category": "mouse",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "Logitech G502 Pro",
         "description": "The best mouse in the world",
         "price": 39.99,
         "stock": 12,
-        "category": "mouse"
+        "category": "mouse",
+        "imgUrl": "http://defaultimage.com"
     },
     {
         "name": "SteelSeries Rival 3",
         "description": "The best mouse in the world",
         "price": 29.99,
         "stock": 12,
-        "category": "mouse"
+        "category": "mouse",
+        "imgUrl": "http://defaultimage.com"
     }
 ]
